d1af1d99e6c25eb6b54c222f88209169c09e1b66
hamad-alzayani-nv22021 <154792345+hamad-alzayani-nv22021@users.noreply.github.com>
2024-02-27 09:41:23 +0000

    video3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,18 @@ import { images } from '../constants'
 const Header = () => {
   return (
     <section>
-        <header className='container mx-auto px-5 flex justify-between py-4'>
+        <header className='container mx-auto px-5 flex justify-between py-4 items-center' >
             <div>
                 <img src={images.Logo} alt="logo" />
             </div>
-            <div className='flex gap-x-9'>
-                <ul className='flex gap-x-5'>
-                    <li>
-                        <a href='/'>Home</a>
+            <div className="flex gap-x-9 items-center">
+                <ul className="flex gap-x-5 font-semibold">
+                    <li className="relative group">
+                        <a href="/" className='px-4 py-2 ' >Home</a>
+                        <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0  group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
                     </li>
                     <li>
-                        <a href='/'>Articals</a>
+                        <a href='/'>Articles</a>
                     </li>
                     <li>
                         <a href='/'>Pages</a>
@@ -26,7 +27,7 @@ const Header = () => {
                         <a href='/'>Faq</a>
                     </li>
                 </ul>
-                <button className='bg-blue-500'>Sign in</button>
+                <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
             </div>
         </header>
     </section>

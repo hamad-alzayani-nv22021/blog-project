b42994338a0e4529f4675dd29b8683a7cb5130f2
hamad-alzayani-nv22021 <154792345+hamad-alzayani-nv22021@users.noreply.github.com>
2024-04-25 09:41:12 +0000

    MERN stack Blog app using Tailwind CSS - 19 - implementing Redux

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import MainLayout from "../../components/MainLayout";
-import Articles from "./Articles";
-import CTA from "./CTA";
-import Hero from "./Hero";
+import Articles from "./container/Articles";
+import CTA from "./container/CTA";
+import Hero from "./container/Hero";
 
 const HomePage = () => {
   return (
